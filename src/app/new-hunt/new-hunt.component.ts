4aa27e7343ee25c9447642b138f16999b34119d8
Poi <poidealings@gmail.com>
2023-12-03 23:27:56 -0500

    feat(): added new trigger button, fixed styling

--- src/app/new-hunt/new-hunt.component.ts
+++ src/app/new-hunt/new-hunt.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexContainerComponent } from '../shared/flex-container/flex-container.component';
 
 @Component({
   selector: 'new-hunt',
   standalone: true,
   imports: [
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule,
+    FlexContainerComponent,
   ],
   templateUrl: './new-hunt.component.html',
   styleUrl: './new-hunt.component.scss'
 })
 export class NewHuntComponent {
-  onNewHunt(): void {
-    
-  }
+  huntName: string = '';
+
+  onAddNewTrigger(): void {}
 }
---
+++ src/app/shared/flex-container/flex-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flex-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './flex-container.component.html',
+  styleUrl: './flex-container.component.scss'
+})
+export class FlexContainerComponent {
+
+}
